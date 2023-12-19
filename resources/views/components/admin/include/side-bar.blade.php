bf323cf5482e79421da6c416e8b062660f28d146
chahal1997 <surinder.singh@echttech.com>
2023-12-19 18:01:34 +0530

    update

--- resources/views/components/admin/include/side-bar.blade.php
+++ resources/views/components/admin/include/side-bar.blade.php
@@ -105,7 +105,7 @@
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="menu-item">
                     <div class="menu-content pt-8 pb-0">
                         <span class="menu-section text-muted text-uppercase fs-8 ls-1">Settings</span>
